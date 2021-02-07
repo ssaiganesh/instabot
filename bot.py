d58a0da94b224626f68f539a65f597ed5095d273
ssaiganesh <shankarsaiganesh@gmail.com>
2021-02-07 12:15:08 +0800

    bot.py added

---
+++ bot.py
@@ -0,0 +1,19 @@
+from instabot import Bot
+import glob, os
+from login import myusername, mypassword
+#### remove json file 
+
+try:
+    os.remove('config')
+except:
+    pass
+
+bot = Bot()
+
+bot.login(username = myusername, password = mypassword)
+os.chdir("images")
+for file in glob.glob("*.jpg"):
+    name = os.path.splitext(os.path.basename(file))[0]
+    with open(f'{name}.txt') as text:
+        contents = text.read()
+    bot.upload_photo(file, caption = contents)
