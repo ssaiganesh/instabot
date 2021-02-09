b403bd2b985d709901dddb241dc25d449e18beab
ssaiganesh <shankarsaiganesh@gmail.com>
2021-02-10 00:08:25 +0800

    README changes

--- README.md
+++ README.md
@@ -1,8 +1,3 @@
 # instabot
 Automating Posts with bot
 
-Example of how it works:
-
-![Inputing query and number of images](2.jpg)
-
-![Final Results](1.jpg)
--- bot.py
+++ bot.py
@@ -1,7 +1,9 @@
 from instabot import Bot
 import glob, os 
 from login import myusername, mypassword #requires separate python file
+# need to create multiple functions now for album and single photo
 
+"""
 #### remove json file 
 try:
     os.remove('config')
@@ -17,3 +19,25 @@ for file in glob.glob("*.jpg"):
     with open(f'{name}.txt') as text:
         contents = text.read()
     bot.upload_photo(file, caption = contents)
+
+"""
+
+
+#### remove json file 
+try:
+    os.remove('config')
+except:
+    pass
+
+bot = Bot()
+
+bot.login(username = myusername, password = mypassword)
+os.chdir("images")
+
+image_list = []
+for file in glob.glob("*.jpg"):
+    image_list.append(os.path.basename(file))
+#name = os.path.splitext(os.path.basename(image_list[-1]))[0]
+for text in glob.glob("*.txt"):
+        contents = text.read()
+bot.upload_album(image_list, caption = contents)
